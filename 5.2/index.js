c76ae7e3012624f28d4db8ca6559ed3bf85fe457
Adrián <adriangalende@gmail.com>
2019-02-16 10:58:57 +0100

    5.2 - b

--- 5.2/index.js
+++ 5.2/index.js
@@ -1,16 +1,25 @@
 window.onload = function () {
+    var parrafos = document.getElementById("info").querySelectorAll("p");
     //Controlamos el movimiento del ratón
     ["mouseover","mouseout"].forEach(function(action){
         window.addEventListener(action, function(pointer){
-            document.getElementById("info").querySelectorAll("p").forEach(function(puntero, indice){
+            parrafos.forEach(function(puntero, indice){
                 if(indice == 0){
                     puntero.innerHTML = modificarTextoCoordenadas(puntero, pointer.screenX, pointer.screenY)
                 } else if (indice == 1){
-                    puntero.innerHTML = modificarTextoCoordenadas(puntero, pointer.clientX, pointer.clientY)
+                    puntero.innerHTML = modificarTextoCoordenadas(puntero, pointer.pageX, pointer.pageY)
+                } else {
+                    //no hacer nada
                 }
             })
         }, false);
-    })
+    });
+
+    //Controlamos los clicks del ratón
+    window.onmousedown = function(target){
+        parrafos[3].innerHTML = target.button == 0? "Clic esquerre Si":"Clic esquerre No";
+        parrafos[4].innerHTML = target.button == 2? "Clic dret Si":"Clic dret No";
+    };
 
     function modificarTextoCoordenadas(puntero,x,y){
         return puntero.innerHTML.split(" ")[0] + " [" + x + ", " + y +"] ";
