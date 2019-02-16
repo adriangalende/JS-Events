9a5105c6d335097383132b2747291d965422e6ab
Adrián <adriangalende@gmail.com>
2019-02-16 17:58:17 +0100

    5.4

--- 5.3/index.js
+++ 5.3/index.js
@@ -4,4 +4,16 @@ window.onload = function(){
                 document.getElementById("footer").innerHTML = "This is the Footer - Mouse over: " + evento.fromElement.id
             }
         }, false)
+
+        //Capturamos el evento onClick en el div wrapper
+        document.getElementById("wrapper").addEventListener("click", detecta_zona,false);
+
+        function detecta_zona(puntero){
+            puntero.path.forEach(function(path){
+                if(path.id != undefined && path.id != "" && path.id != "wrapper"){
+                    alert(path.id);
+                    return;
+                }
+            })
+        }
 }
