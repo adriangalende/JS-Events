22dae2d1e4b9e366975990465232afbc8448e166
Adrián <adriangalende@gmail.com>
2019-02-16 11:22:35 +0100

    5.2 - c y d

--- 5.2/index.js
+++ 5.2/index.js
@@ -1,8 +1,12 @@
 window.onload = function () {
-    var parrafos = document.getElementById("info").querySelectorAll("p");
-    //Controlamos el movimiento del ratón
+    var info = document.getElementById("info");
+    var parrafos = info.querySelectorAll("p");
+    var h1 =  info.children[0];
+        //Controlamos el movimiento del ratón
     ["mouseover","mouseout"].forEach(function(action){
         window.addEventListener(action, function(pointer){
+            cambiarTitulo("Ratolí")
+            info.style = "background-color:#fff";
             parrafos.forEach(function(puntero, indice){
                 if(indice == 0){
                     puntero.innerHTML = modificarTextoCoordenadas(puntero, pointer.screenX, pointer.screenY)
@@ -17,6 +21,8 @@ window.onload = function () {
 
     //Controlamos los clicks del ratón
     window.onmousedown = function(target){
+        cambiarTitulo("Ratolí")
+        info.style = "background-color:#ffffdd";
         parrafos[3].innerHTML = target.button == 0? "Clic esquerre Si":"Clic esquerre No";
         parrafos[4].innerHTML = target.button == 2? "Clic dret Si":"Clic dret No";
     };
@@ -25,4 +31,26 @@ window.onload = function () {
         return puntero.innerHTML.split(" ")[0] + " [" + x + ", " + y +"] ";
     }
 
+    function cambiarTitulo(nuevoTitulo){
+        h1.innerHTML = nuevoTitulo;
+    }
+
+
+    //controlamos acciones con teclado
+    ["keydown","keyup"].forEach(function(action){
+        window.addEventListener(action, function(key){
+            cambiarTitulo("Teclat")
+            info.style = "background-color:#8db6bf";
+            parrafos.forEach(function(parrafo, indice){
+                if(indice == 0){
+                    parrafo.innerText = "Caracter ["+key.key+"]";
+                } else if (indice == 1){
+                    parrafo.innerText = "Codi ["+key.which+"]";
+                } else {
+                    parrafo.innerHTML = "";
+                }
+            })
+
+        }, false);
+    });
 }
